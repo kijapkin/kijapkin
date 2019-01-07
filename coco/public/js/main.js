001736ea35b2ce8d77fa3c85da8bbd0405835302
kijapkin <kijapkin@gmail.com>
2019-01-07 11:55:36 +0200

    Initial commit

---
+++ coco/public/js/main.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function(){
+	
+
+	/*if(jQuery('.slider-gallery').lenght != 0){
+		jQuery('.slider-gallery').slick({
+			arrows: false,
+      centerMode: false,
+      slidesToShow: 2,
+			draggable:true,
+			responsive: [
+		    {
+		      breakpoint: 1200,
+		      settings: {
+						arrows: false,
+						centerMode: true,
+						slidesToShow: 2,
+						draggable:true,
+		      }
+	    	}
+    	]
+		});*/
+		
+
+/*	//Определяем карту, координаты центра и начальный масштаб
+	var map = L.map('map').setView([55.870536, 37.405240], 15);
+
+	L.marker([55.870536, 37.405240]).addTo(map) //добавление маркера на карту
+		.bindPopup("<strong>HELIMKAD-GREENWOOD</strong>").openPopup();
+	//Добавляем на нашу карту слой OpenStreetMap
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    //attribution: '&copy; <a rel="nofollow" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+};*/ 
+
+});
